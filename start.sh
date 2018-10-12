9644113d2f9ab5dfcd2e86f1463594a36b5a5f52
Erik Hebisch <flegelleicht@gmail.com>
2018-10-12 15:23:32 +0200

    Removed start script

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-JWT_SECRET= JWT_ISSUER= bundle exec rackup -s thin -p 4567 -o 0.0.0.0
+JWT_SECRET=huiH9I6k3B8WfCQO1QzFNw JWT_ISSUER=net.taler bundle exec rackup -s thin -p 4567 -o 0.0.0.0
