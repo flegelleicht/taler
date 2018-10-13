9faec18258507d24a3a79cae8820e4438d2c7621
Erik Hebisch <flegelleicht@gmail.com>
2018-10-13 11:16:32 +0200

    Added login component to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Login from './components/Login';
 
 class App extends Component {
   render() {
     return (
       <div>
         <p>Hello World</p>
+        <Login />
       </div>
     );
   }
