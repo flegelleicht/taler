1bb1e6dc217cb4138f9d64c2183540ff8fd3db37
Erik Hebisch <flegelleicht@gmail.com>
2018-10-13 12:17:54 +0200

    Changed order of imports (cosmetic)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import { Provider } from 'react-redux';
 
 import rootReducer from './reducers';
-import * as serviceWorker from './serviceWorker';
-
 import { LoginListener } from './listeners/LoginListener';
 
 import './index.css';
 import App from './App';
-
 
 const loggerMiddleware = createLogger();
 const store = createStore(
