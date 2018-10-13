194d3ec956197de9d6fba1bb9378c3361ee3ba6b
Erik Hebisch <flegelleicht@gmail.com>
2018-10-13 14:31:31 +0200

    Added requesting budgets to the budget component

--- frontend/src/components/Budgets.js
+++ frontend/src/components/Budgets.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { fetchBudgetsRequest } from '../actions';
 
 class Budgets extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(fetchBudgetsRequest());
+  }
+  
   render() {
+    const budgets = this.props.budgets.map(
+      b => <li key={b.id}>{b.name}</li>
+    );
     return(
-      <h3>Budgets</h3>
+      <ul>
+        {budgets}
+      </ul>
     );
   }
 }
 
 const mapStateToProps = state => ({
+  budgets: state.data.budgets
 });
 
 
