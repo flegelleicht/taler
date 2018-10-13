8e85f3318a9ee45417c3753de68b7f22df7e0938
Erik Hebisch <flegelleicht@gmail.com>
2018-10-13 19:00:44 +0200

    Added budget component to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,24 +4,36 @@ import { connect } from 'react-redux';
 import './App.css';
 import Login from './components/Login';
 import Budgets from './components/Budgets';
+import Budget from './components/Budget';
 
 class App extends Component {
   render() {
+    
+    let screen;
+    switch (this.props.display) {
+    case 'budgets':
+      screen = <Budgets />;
+      break;
+    case 'budget':
+      screen = <Budget />;
+      break;
+    default:
+      screen = null
+      break;
+    }
+    
     return (
-      <div>
+      <React.Fragment>
         <Login />
-        {this.props.loggedIn === true ?
-          <Budgets />
-        :
-          null
-        }
-      </div>
+        {this.props.loggedIn ? screen : null}
+      </React.Fragment>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  loggedIn: state.login.loggedIn
+  loggedIn: state.login.loggedIn,
+  display: state.ui.display
 });
 
 
